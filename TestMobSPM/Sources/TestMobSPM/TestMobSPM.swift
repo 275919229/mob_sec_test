8e7c06b801961efd2b51f89ba55b71c8828ccb9b
howerzhao <275919229@qq.com>
2025-09-22 11:58:12 +0800

    test1

---
+++ TestMobSPM/Sources/TestMobSPM/TestMobSPM.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import Foundation
+// 如果依赖了Alamofire，记得import
+// import Alamofire
+
+public struct NetworkingManager {
+    public static func doSomethingAwesome() {
+        print("Something awesome is happening!")
+    }
+
+    // 必须显式声明 public 的初始化方法，外部才能访问
+    public init() {}
+}
