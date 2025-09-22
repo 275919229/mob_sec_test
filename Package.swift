171adeceec19361668571abf2c8d83ad022cb70b
howerzhao <275919229@qq.com>
2025-09-22 15:21:57 +0800

    v1.0.3

--- Package.swift
+++ Package.swift
@@ -5,17 +5,36 @@ import PackageDescription
 
 let package = Package(
     name: "mob_sec_test",
+    platforms: [.iOS(.v12)], // 匹配OC Framework支持的最低版本
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "mob_sec_test",
             targets: ["mob_sec_test"]),
     ],
+    dependencies: [
+        //.package(path: "../Frameworks/FlyVerifyCSDK.xcframework"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "mob_sec_test_oc_fly",
+                      path: "Frameworks/FlyVerifyCSDK.xcframework"),
+        .binaryTarget(name: "mob_sec_test_oc_mob",
+                      path: "Frameworks/MOBFoundation.xcframework"),
         .target(
-            name: "mob_sec_test"),
+            name: "mob_sec_test",
+            dependencies: ["mob_sec_test_oc_fly","mob_sec_test_oc_mob"],
+            linkerSettings: [
+                .linkedLibrary("z"),
+                .linkedLibrary("c++"),
+                .linkedFramework("Security"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("UIKit"),
+                .unsafeFlags(["-ObjC"])
+            ]
+        ),
         .testTarget(
             name: "mob_sec_testTests",
             dependencies: ["mob_sec_test"]
--- Tests/mob_sec_testTests/mob_sec_testTests.swift
+++ Tests/mob_sec_testTests/mob_sec_testTests.swift
@@ -1,5 +1,8 @@
 import XCTest
+
+
 @testable import mob_sec_test
+import MOBFoundation
 
 final class mob_sec_testTests: XCTestCase {
     func testExample() throws {
@@ -8,5 +11,15 @@ final class mob_sec_testTests: XCTestCase {
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        
+        NetworkingManager.doSomethingAwesome()
+        //FlyVerifyC.initKey("123", secret: "321")
+//        let value1 = FlyVerifyC.flyVerifyKey()
+        MobSDK.registerAppKey("123", appSecret:  "321")
+        let value1 = MobSDK.appKey()
+        print(value1)
+        
+        
+        
     }
 }
